c5bccab9bbd1e333ce1de2e152fd463ce1e508bd
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-24 21:17:06 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def line(array)
+  case array.size
+    when 0
+      puts "The line is currently empty."
+    end
+
+
+end
+
+def take_a_number
+
+
+end
+
+def now_serving(array)
+  case array.size
+  when 0
+    puts "There is nobody waiting to be served!"
+  end
+
+end
