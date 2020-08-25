12b764c90031333428235e211205a1adec3579d4
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-25 01:14:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,20 +2,25 @@ def line(array)
   case array.size
     when 0
       puts "The line is currently empty."
+    else
+      final = "The line is currently:"
+      array.each.with_index(1) do |customer, index|
+final += " #{index}. #{customer}"
+      end
+      puts final
     end
-
-
 end
 
-def take_a_number
-
-
+def take_a_number(line, name)
+  line.push(name)
+  puts "Welcome, #{name}. You are number #{line.length} in line."
 end
 
-def now_serving(array)
-  case array.size
-  when 0
+def now_serving(line)
+ if line.length == 0
     puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}."
+    line.shift
   end
-
 end
