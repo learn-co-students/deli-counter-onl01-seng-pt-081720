310478b05205eb7bb2a49c8a0cba895209125407
Myles Janik <mylesjanik@gmail.com>
2020-08-30 22:33:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,13 @@
 # Write your code here.
 
 def line(array)
+  counter = 0
   if array.length == 0
     puts "The line is currently empty."
+  else
+    temp_string1 = "The line is currently: "
+    while counter < array.length
+      
+    end
   end
 end
